2776c32c756d92f22938355cb96e60383fa3ae55
MelissaOfficina <95360353+MelissaOfficina@users.noreply.github.com>
2021-12-02 18:22:59 +0400

    Update day1.py
    The previous solution could have been buggy :^)

--- day1.py
+++ day1.py
@@ -15,11 +15,11 @@ def check_previous_extended(some_list):
     counter = 0
     previous = False
     for key, el in enumerate(some_list):
-        if el != some_list[-2] and el != some_list[-1]:
+        if key != (len(some_list) - 1) and key != (len(some_list) - 2):
             sum_ = el + some_list[key + 1] + some_list[key + 2]
-        elif el == some_list[-2]:
+        elif key == (len(some_list) - 2):
             sum_ = el + some_list[key + 1]
-        elif el == some_list[-1]:
+        elif key == (len(some_list) - 1):
             sum_ = el
         if previous is not False and sum_ > previous:
             counter += 1
